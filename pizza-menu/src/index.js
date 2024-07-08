60ad8c4e8e4c0ce29638b20a920e8271f26907f6
Konstantin Gross <gross.konstantin.1987@gmail.com>
2024-07-08 23:55:38 +0200

    started with props

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -71,11 +71,33 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our menu</h2>
-      <Pizza />;
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Pizza
+        name="Pizza Spinaci"
+        ingredient="Tomato, mozarella, spinach, and ricotta cheese "
+        photoName="pizzas/spinaci.jpg"
+        price={10}
+      />
+      <Pizza
+        name="Pizza Funghi"
+        ingredient="Tomato, mozarella, mushrooms, and onion"
+        photoName="pizzas/funghi.jpg"
+        price={12}
+      />
+      ;
     </main>
+  );
+}
+
+function Pizza(props) {
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredient}</p>
+        <span>{props.price + 3}</span>
+      </div>
+    </div>
   );
 }
 
@@ -85,20 +107,8 @@ function Footer() {
   const openHour = 12;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
-  console.log(isOpen);
 
-  // return React.createElement('footer', null, `We're currently open!`);
-  return <footer className="footer">{new Date().toLocaleTimeString()}. We're currently open</footer>;
-}
-
-function Pizza() {
-  return (
-    <div>
-      <h3>Pizza Spinaci</h3>
-      <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci" />
-      <p>Tomato, mozarella, spinach, and ricotta cheese </p>
-    </div>
-  );
+  return <footer className={'container footer'}>{new Date().toLocaleTimeString()}. We're currently open</footer>;
 }
 
 // React 18
