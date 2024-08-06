82b81c1268235ba7f7cb4fd71e97dcc1558b7aa4
Konstantin Gross <gross.konstantin.1987@gmail.com>
2024-08-06 06:42:22 +0200

    Hello from macbook pro

--- steps/src/App.js
+++ steps/src/App.js
@@ -1,10 +1,19 @@
 export default function App() {
+  const step = 1;
+
+  function handlePrevious() {
+    console.log('Previous button clicked');
+  }
+
+  function handleNext() {
+    console.log('Next button clicked');
+  }
   return (
     <div className="steps">
       <div className="numbers">
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
+        <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
+        <div className={`${step >= 2 ? 'active' : ''}`}> 2</div>
+        <div className={`${step >= 3 ? 'active' : ''}`}>3</div>
       </div>
 
       <p className="message">Hello</p>
@@ -15,10 +24,19 @@ export default function App() {
             backgroundColor: '#7950f2',
             color: '#fff',
           }}
+          onClick={handlePrevious}
         >
           Previous
         </button>
-        <button>Next</button>
+        <button
+          style={{
+            backgroundColor: '#7950f2',
+            color: '#fff',
+          }}
+          onClick={handleNext}
+        >
+          Next
+        </button>
       </div>
     </div>
   );
